02c3f04a130bad54f35903b5efd8db3c56e44453
Rob J Hyndman <robjhyndman@gmail.com>
2015-08-13 14:47:00 +1000

    Updated gams example for lecture 6

--- slides/3-gams.R
+++ slides/3-gams.R
@@ -11,13 +11,29 @@ library(gam)
 plot.gam(fit2)
 
 library(ggplot2)
-qplot(age, wage, data = Wage) + geom_smooth()
+qplot(age, wage, data = Wage) + 
+  geom_smooth()
 
 qplot(age, wage, data = Wage) + geom_smooth() + facet_wrap(~ education)
 
 qplot(age, wage, data = Wage) + geom_smooth() + facet_wrap(~ year)
 
-qplot(age, wage, data = Wage) + geom_smooth() + facet_wrap(~ education + year)
 
+# Conditioning with a numerical variable
 
+qplot(horsepower, mpg, data=Auto) + geom_smooth()
+
+qplot(horsepower, mpg, data=Auto) + geom_smooth() + facet_wrap(~ cylinders)
+
+Auto$cut <- cut(Auto$year, breaks=3)
+qplot(horsepower, mpg, data=Auto) + geom_smooth() + facet_wrap(~ cut)
+
+Auto$cut <- cut(Auto$displacement, breaks=3)
+qplot(horsepower, mpg, data=Auto) + geom_smooth() + facet_wrap(~ cut)
+
+Auto$cut <- cut(Auto$weight, breaks=3)
+qplot(horsepower, mpg, data=Auto) + geom_smooth() + facet_wrap(~ cut)
+
+Auto$cut <- cut(Auto$acceleration, breaks=3)
+qplot(horsepower, mpg, data=Auto) + geom_smooth() + facet_wrap(~ cut)
 
