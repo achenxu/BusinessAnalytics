0ba4d63410c087f32c8050bb5fa352872764e689
Souhaib BenTaieb <souhaibt@dyn-130-194-61-84.its.monash.edu.au>
2015-08-20 10:11:45 +1000

    final update of code

--- slides/4-bootstrap-sbt.R
+++ slides/4-bootstrap-sbt.R
@@ -2,40 +2,52 @@ rm(list = ls())
 library(ggplot2)
 set.seed(1986)
 
-b.theta <- function(data, B, s = mean) {
-  bootstrap_samples <- lapply(1:B, function(i) sample(data, replace=T))
-  thetas <- sapply(bootstrap_samples, s)
-  std.err <- sd(thetas)
-  list(bootstrap_samples = bootstrap_samples, thetas = thetas, std.err = std.err)   
-}
-
 n <- 10000
-B <- 100
 
-# Normal distribution 
+# Data generated from normal distribution  
 mu <- 4
 sigma <- 2
-data <- rnorm(n, mean = mu, sd = sigma)
-qplot(data) 
+data_normal <- rnorm(n, mean = mu, sd = sigma)
+qplot(data_normal) 
 
-b.obj <- b.theta(data, B)
-boot_std.err <- b.obj$std.err
-theo_std.err <- sigma / sqrt(n)
-print(paste(theo_std.err, " - ", boot_std.err, sep = ""))
-
-qplot(b.obj$thetas) + geom_vline(xintercept = mean(data), col='blue') +
-  geom_vline(xintercept = mu, col='red')
-
-
-# More complex distribution
+# Data generated from a more complex distribution
 occurence <- rbinom(n, 1, prob = 0.7) 
 intensity <- rgamma(n, shape = 2, scale = 2)
-data <- occurence*intensity
-qplot(data) 
+data_complex <- occurence*intensity
+qplot(data_complex) 
 
-b.obj <- b.theta(data, B, median)
+b.theta <- function(data, B, s) {
+  thetas_hat <- NULL
+  for(b in seq(B)){
+    bootstrap_sample <- sample(data, replace = T)
+    theta_hat <- s(bootstrap_sample)
+    thetas_hat <- c(thetas_hat, theta_hat)
+  }
+  std.err <- sd(thetas_hat)
+  list(std.err = std.err, thetas_hat = thetas_hat)
+}
+
+#### Better function ####
+# b.theta <- function(data, B, s) {
+#  bootstrap_samples <- lapply(seq(B), function(i) sample(data, replace=T))
+#  thetas_hat <- sapply(bootstrap_samples, s)
+#  std.err <- sd(thetas_hat)
+# list(std.err = std.err, thetas_hat = thetas_hat)
+# }
+
+B <- 100
+
+# Standard errors for sample mean
+b.obj <- b.theta(data_normal, B, mean)
+boot_std.err <- b.obj$std.err
+theo_std.err <- sigma / sqrt(n)
+c(theo_std.err, boot_std.err)
+qplot(b.obj$thetas_hat) + geom_vline(xintercept = mean(data_normal), col='blue') +
+  geom_vline(xintercept = mu, col='red')
+
+# Standard errors for sample median
+b.obj <- b.theta(data_complex, B, median)
 boot_std.err <- b.obj$std.err
 print(boot_std.err)
-
-qplot(b.obj$thetas) + geom_vline(xintercept = median(data), col='blue')
+qplot(b.obj$thetas_hat) + geom_vline(xintercept = median(data_complex), col='blue')
 
